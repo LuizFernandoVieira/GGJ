2cbab0ce7fbe98b81bd7e00c0fc589f73d08f835
luiz <luizfernandovieira_cf@hotmail.com>
2016-01-29 21:38:58 -0200

    menu

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuHandler : MonoBehaviour {
+
+    public void PlayClicked()
+    {
+        Application.LoadLevel(1);
+    }
+    
+    public void ExitClicked()
+    {
+        Application.Quit();
+    }
+    
+}
